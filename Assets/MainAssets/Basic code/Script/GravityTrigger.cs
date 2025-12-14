f0e5938861484a0b25528d8514d4db45e3c9bac4
zyc-2024 <61992011@qq.com>
2025-12-14 14:33:22 +0800

    10%

---
+++ Assets/MainAssets/Basic code/Script/GravityTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GravityTrigger : MonoBehaviour
+{
+    public Vector3 newGravity = new Vector3(0, -9.81f, 0);
+
+
+    private Vector3 originalGravity;
+
+    void Start()
+    {
+        originalGravity = Physics.gravity;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Physics.gravity = newGravity;
+
+    }
+
+
+}
